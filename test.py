778631284f9171380ce70190c0cde838b0447d35
jerson carranza lucas <jerson_carranza2@hotmail.com>
2015-05-23 23:53:18 -0500

    avanze 3

--- test.py
+++ test.py
@@ -34,10 +34,9 @@ class Lista:
 			return False
 
 	def Insertar(self,J1,J2,Meta):
-		self.AJ1= J1
-		self.AJ2= J2
+		self.LJ1= J1
+		self.LJ2= J2
 		self.AMet = Meta
-
 		temporal=Nodo(self.LJ1, self.LJ2, self.AMet)
 
 		if self.vacia()==True:
@@ -84,25 +83,46 @@ class Lista:
 			#LJ1 is LJ1 -1
 			#LJ2 is LJ2 + 1
 			#J1aJ2()
-	#pasar agua del jarro2 al jarro1
-	def J2aJ1():
-		if self.LJ2>0 and self.LJ1 < self.J1:
-			LJ2 is LJ2 -1
-			LJ1 is LJ1 + 1
-			J2aJ1()
 	
+	#Utilizar los auxiliares
+	def UtiAux(self,J1,J2,Meta):
+		print "vamos a salvar valores"
+		self.AJ1= J1
+		self.AJ2= J2
+		self.AMet = Meta
+
+		#Genera el primer estado 
+		self.Insertar(0,0,self.AMet)
+
 	#Evento 1 llenar jarro 1
 	def Evento1(self):
 		self.LJ1 = self.AJ1
 		print "******El jarro 1 tiene", self.AJ1, "Litros."
-		self.Insertar(self.AJ1, self.AJ2, self.AMet)
+		self.Insertar(self.LJ1, self.LJ2, self.AMet)
 
 	#Evento 2 llenar jarro 2
 	def Evento2(self):
 		self.LJ2 = self.AJ2
 		print "******El jarro 2 tiene", self.AJ2, "Litros."
-		self.Insertar(self.AJ1, self.AJ2, self.AMet)
+		self.Insertar(self.LJ1, self.LJ2, self.AMet)
 		
+	#Evento 3 pasar agua del jarro2 al jarro1
+	def Evento3(self):
+		print "LJ1 es ",self.LJ1
+		print "AJ1 es ",self.AJ1
+		print "LJ2 es ",self.LJ2
+		if self.LJ2>0 and self.LJ1 < self.AJ1:
+			self.LJ2 = self.LJ2 -1
+			self.LJ1 = self.LJ1 + 1
+			self.Evento3()
+		else:
+			self.Insertar(self.LJ1, self.LJ2, self.AMet)
+
+	#Evento 5 vaciar el jarro 1
+	def Evento5(self):
+
+		self.LJ1=0
+		self.Insertar(self.LJ1, self.LJ2, self.AMet)
 
 	#nos dice si llego a la meta
 	def FunMeta(self,Meta):
@@ -113,13 +133,21 @@ class Lista:
 	#nos regresa un nuevo estado
 	def CrearEstado(self):
 		self.Evento1()#que llene de jarro 1
-		self.Evento2()#que llene el jarro2
-		
+		#if self.FunMeta(self.AMet) == False:
+		#	self.Evento2()#que llene el jarro2
+		#else:
+		#	print "llego a la meta"
+		self.Evento2()
+		self.Evento5()
+		self.Evento3()
+
 listas = Lista()
 ja1 = 5 # capacidad que tiene  el jarro 1
 ja2 = 3 # capacidad que tiene  el jarro 2
-meta = 1 #meta litros agua
-listas.Insertar(ja1,ja2,meta)
+meta = 5 #meta litros agua
+
+listas.UtiAux(ja1,ja2,meta)
+#listas.Insertar(ja1,ja2,meta)
 
 if True == listas.FunMeta(meta):
 	print ("llego a la meta.. ya no haga mas hijos")
